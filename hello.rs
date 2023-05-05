1eb93acda6a153f73660a86b3dacb9006f8737c2
wangjunjia <wang.junjia.dev@gmail.com>
2023-05-05 21:10:30 +0800

    feat: study comment

--- hello.rs
+++ hello.rs
@@ -1,4 +1,10 @@
 // 单行注释
 fn main() {
+    /**
+     *  块注释，/* 可以嵌套 */
+     */
+    let x = 5 + /* 90 + */ 5;
+    // {}, {x} , 都可以
+    println!("Is `x` 10 or 100 ? x = {x}");
     println!("Hello World~");
 }
