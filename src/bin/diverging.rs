d993b064b11b48aec1e27965d5b0b2d981fb5f7c
wangjunjia <wang.junjia.dev@gmail.com>
2023-05-06 19:37:04 +0800

    feat: 整理项目结构

---
+++ src/bin/diverging.rs
@@ -0,0 +1,11 @@
+/**
+ * 发散函数: 绝不返回，使用 ! 标记, 是空类型， 不是 () 单元元组
+ * 类似空集，不是0
+ *
+ */
+
+#![feature(never_type)]
+fn main() {
+    let x:! = panic!("this call never returns");
+    // println!("you will never see this line!");
+}
